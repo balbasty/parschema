73a256a7b7cc6fe1e859a980060889665c8c2bf6
Yael Balbastre <ybalbastre@mgh.harvard.edu>
2023-11-14 20:12:22 -0500

    Move decorator to its own file

--- parschema/__init__.py
+++ parschema/__init__.py
@@ -4,5 +4,7 @@ from .yaml_validate import *    # noqa: F401, F403
 from .cli_parse import *        # noqa: F401, F403
 from .cli_help import *         # noqa: F401, F403
 from .actions import *          # noqa: F401, F403
+from .checks import *           # noqa: F401, F403
+from .decorators import *       # noqa: F401, F403
 from . import actions           # noqa: F401
 from . import checks            # noqa: F401
--- parschema/cli_parse.py
+++ parschema/cli_parse.py
@@ -1,74 +1,8 @@
 __all__ = [
     'schema2cli',
-    'command',
 ]
-import os
-import yaml
-from functools import wraps
 from .cli_help import schema2help
 from .yaml_validate import populate
-
-
-def command(schema=None):
-    """A decorator that transforms a function a hybrid fuction/CLI
-
-    By default, the decorator looks for a yaml file located next to
-    current file
-    ```
-    |-- folder
-      |-- my_script.py
-      |-- segment.yaml
-    ```
-    ```python
-    @command
-    def segment(params):
-        # do something with `params`, which is a dictionary of parameters
-        ....
-
-    segment(['--do-something'])
-    ```
-
-    It is also possible to pass a schema explicitly:
-    ```python
-    @command('path/to/schema.yaml')
-    def segment(params):
-        # do something with `params`, which is a dictionary of parameters
-        ....
-
-    with open('path/to/schema.yaml') as f:
-        schema = yaml.load(f)
-
-    @command(schema)
-    def register(params):
-        # do something with `params`, which is a dictionary of parameters
-        ...
-    ```
-    """
-    if callable(schema):
-        function = schema
-        fpath = function.__file__
-        fname = function.__name__
-        schema = os.path.join(fpath, fname)
-        if not os.path.exists(schema):
-            raise ValueError(f'Could not find a schema for {function}')
-        return command(schema)(function)
-
-    if isinstance(schema, str):
-        with open(schema) as f:
-            schema = yaml.load(f)
-    parser = schema2cli(schema)
-
-    def decorator(function):
-
-        @wraps(function)
-        def wrapper(params):
-            if not isinstance(params, dict):
-                params = parser(params)
-            return function(params)
-
-        return wrapper
-
-    return decorator
 
 
 def schema2cli(schema):
